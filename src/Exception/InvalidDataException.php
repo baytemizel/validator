bd018c2670e145458a1baa02f916f0b444180b6c
tayfun <tayfun.temizel@bodevoffice.com>
2017-11-06 15:30:42 +0300

    adds some implements about yaml files.

---
+++ src/Exception/InvalidDataException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Baytemizel\Validator\Exception;
+
+
+use Baytemizel\Validator\Service\Translator;
+
+class InvalidDataException extends \Exception
+{
+    public function __construct($langCode = 'tr', Throwable $previous = null)
+    {
+        $translator = new Translator('exception_responses', $langCode);
+        $message = $langCode.' :: '.$translator->getTranslation('412.0200');
+        parent::__construct($message, 412, $previous);
+    }
+}
